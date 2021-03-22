3fb7c705dc45b62ea8c31b110dfa673d803162c7
Lucas Buzetto Tsuchiya <lucasbtsuchiya@gmail.com>
2021-03-21 22:58:59 -0300

    Commit

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from bottle import route, run, request
+
+
 @route('/', method='POST')
 def send():
     assunto = request.forms.get('assunto')
     mensagem = request.forms.get('mensagem')
-    return 'Mensagem enfileirada! Assunto: {} Mensagem {}'.format(
-        assunto, mensagem
-    )
+    return 'Mensagem enfileirada! Assunto: {} Mensagem {}'.format(assunto,
+            mensagem)
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080, debug=True)
